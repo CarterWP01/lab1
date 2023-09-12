2a4559906fe1a86ad0a2c85bcf694e0ba3848dd7
CarterWP01 <zekewp101@gmail.com>
2023-09-11 18:43:12 -0600

    2nd commit

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
-https://www.google.ca/import requests
+import requests
 print(requests.__version__)
 #print(requests.get("the githubURL"))
 
 print(requests.get("https://google.ca"))
+url = "https://raw.githubusercontent.com/CarterWP01/lab1/main/script.py"
+res = requests.get(url)
+print(res.text)
